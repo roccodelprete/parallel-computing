4a04cf7162596db06d1d239cb91455ecbe0ca262
Rocco Del Prete <122788299+roccodelprete@users.noreply.github.com>
2024-06-18 22:26:57 +0200

    fix exercise 5

--- exercises/matrix-rows-extraction-exe-5/main.c
+++ exercises/matrix-rows-extraction-exe-5/main.c
@@ -3,25 +3,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
-* Implementare un programma parallelo per l’ambiente
-* multicore con np unità processanti che impieghi la
-* libreria OpenMP. Il programma deve essere
-* organizzato come segue: il core master deve generare
-* una matrice di dimensione N×N, ogni core deve
-* estrarre N/np righe e calcolare il prodotto puntuale tra
-* i vettori corrispondenti alle righe estratte.
- */
+void fillMatrix(int **matrix, const int rows, const int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = 1 + rand() % 10;
+        }
+    }
+}
 
-struct returnType {
-    int **results;
-    double time;
-};
-
-int** matrixAllocation(const int rows, const int cols) {
-    int **matrix = calloc(rows, sizeof(int*));
+int **matrixAllocation(const int rows, const int cols, const int hasToFill) {
+    int **matrix = calloc(rows, sizeof(int *));
     for (int i = 0; i < rows; i++) {
         matrix[i] = calloc(cols, sizeof(int));
+    }
+
+    if (hasToFill) {
+        fillMatrix(matrix, rows, cols);
     }
 
     return matrix;
@@ -38,14 +35,6 @@ void freeMatrix(int **matrix, const int rows) {
         free(matrix[i]);
     }
     free(matrix);
-}
-
-void fillMatrix(int **matrix, const int rows, const int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = 1 + rand() % 10;
-        }
-    }
 }
 
 void printMatrix(int **matrix, const int rows, const int cols) {
@@ -68,21 +57,13 @@ double getExecutionTime(const double start, const double end) {
     return end - start;
 }
 
-struct returnType extractRows(int **matrix, const int rows, const int cols, const int numThreads) {
+void extractRows(int **matrix, const int rows, const int cols, const int numThreads) {
     int i = 0, j = 0;
 
-    struct returnType out;
-    out.results = matrixAllocation(rows, cols);
-
-    for (int k = 0; k < rows / numThreads; k++) {
-        for (int l = 0; l < cols; l++) {
-            out.results[k][l] = 1;
-        }
-    }
-
     const double startTime = omp_get_wtime();
+    int **results = matrixAllocation(numThreads, cols, 0);
 
-#pragma omp parallel private(i, j) shared(matrix, out, rows, cols, numThreads) num_threads(numThreads)
+#pragma omp parallel private(i, j) shared(matrix, results, cols, numThreads) num_threads(numThreads)
     {
         const int threadID = omp_get_thread_num();
         const int rowsPerThread = rows / numThreads;
@@ -97,28 +78,42 @@ struct returnType extractRows(int **matrix, const int rows, const int cols, cons
             end = start + rowsPerThread;
         }
 
-        for (i = start; i < end; i++) {
+        if (end > rows) {
+            end = rows;
+        }
+
+        for (i = start; i < end - 1; i++) {
             for (j = 0; j < cols; j++) {
-                out.results[i][j] = matrix[i][j] * matrix[i + 1][j];
+                results[threadID][j] = matrix[i][j] * matrix[i + 1][j];
             }
+        }
+
+#pragma omp critical
+        {
+            printf("\nThread %d – Puntual product from row %d to %d: \t", threadID, start, end - 1);
+            printArray(results[threadID], cols);
         }
     }
 
     const double endTime = omp_get_wtime();
 
-    out.time = getExecutionTime(startTime, endTime);
+    const double time = getExecutionTime(startTime, endTime);
 
-    return out;
+    printf("\nExecution time: %f\n", time);
+
+    freeMatrix(matrix, rows);
+    freeMatrix(results, numThreads);
 }
 
 int main(void) {
+    srand(time(NULL));
+
     int rows;
     printf("Insert rows number: ");
     scanf("%d", &rows);
     const int cols = rows;
 
-    int **matrix = matrixAllocation(rows, cols);
-    fillMatrix(matrix, rows, cols);
+    int **matrix = matrixAllocation(rows, cols, 1);
     printf("\nMatrix: \n");
     printMatrix(matrix, rows, cols);
 
@@ -126,14 +121,7 @@ int main(void) {
     printf("\nInsert number of threads: ");
     scanf("%d", &numThreads);
 
-    const struct returnType result = extractRows(matrix, rows, cols, numThreads);
-
-    printf("\nResult: \n");
-    printMatrix(result.results, rows / numThreads, cols);
-
-    printf("\nExecution time: %f\n", result.time);
-
-    freeMatrix(matrix, rows);
+    extractRows(matrix, rows, cols, numThreads);
 
     return 0;
 }
